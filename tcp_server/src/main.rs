54548fb2b761ce548d6e827245a70fd02df3d057
rbnrayan <rayan.robion7@gmail.com>
2022-07-07 12:37:03 +0200

    tcp_server clients_list

--- tcp_server/src/main.rs
+++ tcp_server/src/main.rs
@@ -1,12 +1,13 @@
 use std::net::{TcpListener, TcpStream};
 use std::io;
+use std::sync::{Arc, Mutex};
 use threadpool::{self, ThreadPool};
 use tcp_utils;
 
 const IP: &'static str = "127.0.0.1";
 const PORT: &'static str = "8080";
 
-fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+fn handle_connection(mut stream: TcpStream, clients: Arc<Mutex<Vec<Arc<Mutex<TcpStream>>>>>) -> io::Result<()> {
     let peer_connection = stream.peer_addr()?;
     let read_logfn = |recvd_bytes: &Vec<u8>| println!(
         "[{}:{} |>>|] {{ {} bytes }} :\n\t`{}`",
@@ -23,6 +24,9 @@ fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
         std::str::from_utf8(src).unwrap(),
     );
 
+    let client = Arc::new(Mutex::new(stream.try_clone()?));
+    clients.lock().unwrap().push(Arc::clone(&client));
+
     println!(
         "[Connection established]\n\tIP_ADDR: {}\n\tPORT   : {}",
         peer_connection.ip(), 
@@ -33,14 +37,23 @@ fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
     tcp_utils::log_send_bytes(&mut stream, msg, send_logfn)?;
 
     loop {
-        let read_bytes_res = tcp_utils::log_read_bytes(&mut stream, read_logfn);
-        if let Err(_) = read_bytes_res {
-            println!(
-                "[Connection closed]\n\tIP_ADDR: {}\n\tPORT   : {}",
-                peer_connection.ip(),
-                peer_connection.port()
-            );
-            break;
+        let read_bytes = match tcp_utils::log_read_bytes(&mut stream, read_logfn) {
+            Ok(bytes) => bytes,
+            Err(_) => {
+                println!(
+                    "[Connection closed]\n\tIP_ADDR: {}\n\tPORT   : {}",
+                    peer_connection.ip(),
+                    peer_connection.port()
+                );
+                break;
+            }
+        };
+        
+        let clients_lock = clients.lock().unwrap();
+        let clients = clients_lock.iter();
+        for client in clients {
+            let mut client = client.lock().unwrap();
+            tcp_utils::log_send_bytes(&mut client, &read_bytes, send_logfn)?;
         }
     }
     Ok(())
@@ -50,13 +63,16 @@ fn main() -> io::Result<()> {
     let listener = TcpListener::bind(format!("{}:{}", IP, PORT))?;
     let pool = ThreadPool::new(4, threadpool::Mode::Quiet);
 
+    let clients = Arc::new(Mutex::new(Vec::new()));
+
     println!("[Server info]\n\tIP_ADDR: {}\n\tPORT   : {}\n", IP, PORT);
 
     for stream in listener.incoming() {
         let stream = stream?;
+        let clients = Arc::clone(&clients);
 
         pool.execute(|| {
-            if let Err(e) = handle_connection(stream) {
+            if let Err(e) = handle_connection(stream, clients) {
                 println!("[HANDLE ERROR]:\n\t{}", e);
             }
         });
